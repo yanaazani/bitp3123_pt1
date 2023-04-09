e31565ab0e88d53d7071700fc9b8cca35e09d11b
yanaazani <shaufyy48@gmail.com>
2023-04-09 22:54:49 +0800

    First Commit for Location.java

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
